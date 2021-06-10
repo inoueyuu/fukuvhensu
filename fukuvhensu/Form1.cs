6bccc09384a74e5198a71b74c04a1c74268e7944
A601-13 <82490771+inoueyuu@users.noreply.github.com>
2021-06-10 10:00:35 +0900

    追加

--- fukuvhensu/Form1.cs
+++ fukuvhensu/Form1.cs
@@ -12,6 +12,9 @@ namespace fukuvhensu
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,8 +32,11 @@ namespace fukuvhensu
 
         private void button2_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "-1";
+            //textBox1.Text = "0";
+            //textBox2.Text = "-1";
+            vx = 0;
+            vy = -1;
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -40,27 +46,35 @@ namespace fukuvhensu
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "-1";
-            textBox2.Text = "0";
+            //textBox1.Text = "-1";
+            //textBox2.Text = "0";
+            vx = -1;
+            vy = 0;
 
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "1";
-            textBox2.Text = "0";
+            //textBox1.Text = "1";
+            //textBox2.Text = "0";
+            vx = 1;
+            vy = 0;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            label1.Top += int.Parse(textBox2.Text);
-            label1.Left += int.Parse(textBox1.Text);
+            //label1.Top += int.Parse(textBox2.Text);
+            //label1.Left += int.Parse(textBox1.Text);
+            label1.Top = vy;
+            label1.Left = vx;
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            textBox1.Text = "0";
-            textBox2.Text = "1";
+            //textBox1.Text = "0";
+            //textBox2.Text = "1";
+            vx = 0;
+            vy = 1;
         }
 
         private void label1_Click(object sender, EventArgs e)
